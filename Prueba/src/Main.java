edfc4f0a76775a95ff6b597b7932234d6c78954f
DionisOctavio <dionisoctavioalvarezescorcia@gmail.com>
2024-10-02 09:50:10 +0200

    [02/10/2024] 9:49 Java Num aleatorio
    [02/10/2024] 9:49 Java Num aleatorio

---
+++ Prueba/src/Main.java
@@ -0,0 +1,19 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+import java.util.Random;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Random r = new Random();
+
+        System.out.println("Hola, CO");
+
+
+        for (int i = 1; i <= 5; i++) {
+
+
+            System.out.println("H = " + r.nextInt(100));
+        }
+    }
+}
