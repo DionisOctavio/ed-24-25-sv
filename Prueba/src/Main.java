92a0035e6b8ad5777ce167f784f9db5364b2a418
DionisOctavio <dionisoctavioalvarezescorcia@gmail.com>
2024-10-07 18:08:02 +0200

    [07/10/2024] 18:07 V2.0
    [07/10/2024] 18:07 V2.0

--- Prueba/src/Main.java
+++ Prueba/src/Main.java
@@ -1,19 +1,35 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-
         Random r = new Random();
+        int[] generarNumeros = new int[5];
+        int contador = 0; 
 
-        System.out.println("Hola, CO");
+        System.out.println("Buenas, CO");
 
+        while (contador < 5) { // Mientras
+            int num = r.nextInt(100); 
 
-        for (int i = 1; i <= 5; i++) {
+            boolean esta = false;
 
+            
+            for (int i = 0; i < contador; i++) { //SI
+                if (generarNumeros[i] == num) {
+                    esta = true; 
+                    break;
+                }
+            }
 
-            System.out.println("H = " + r.nextInt(100));
+        
+            if (!esta) {
+                generarNumeros[contador] = num; 
+                System.out.println("H = " + num);
+                contador++; 
+            }
         }
     }
 }
