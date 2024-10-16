42d6a974100c59dd0db6e5bbaa5527b49f902f67
DionisOctavio <dionisoctavioalvarezescorcia@gmail.com>
2024-10-16 09:54:20 +0200

    [16/10/2024] 9:54
    [16/10/2024] 9:54

--- 002 Ejercicio 3 V-2.0/src/cienNumeros/cienNumeros.java
+++ 002 Ejercicio 3 V-2.0/src/cienNumeros/cienNumeros.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+import java.util.Scanner;
 public class cienNumeros {
 
     public static int[] jusepe = new int [100];
@@ -5,16 +7,17 @@ public class cienNumeros {
 
     public static void main(String[] args) {
 
-    int contador = 0;
+        int contador = 0;
+        Random aleatorio = new Random();
+        Scanner leer = new Scanner(System.in);
 
-    for (int i = 0; i < 100; i++) {
-        jusepe[i] = contador;
-    }
-
-    for (int j = 0; j < 100; j++) {
-        System.out.println(jusepe[j]);
-    }
+        do {
+            jusepe[contador] = aleatorio.nextInt(1000);
+            contador = contador + 1;
 
 
+        }while(1<100);
+
+        x = leer.nextInt();
     }
 }
