635fd8615f2d95ce7c3af294dfaa93ae25442330
DionisOctavio <dionisoctavioalvarezescorcia@gmail.com>
2025-01-21 22:55:43 +0100

    [21/01/2025] 22:53
    [21/01/2025] 22:53

--- 005 UML Sistema Lavadora/src/Main.java
+++ 005 UML Sistema Lavadora/src/Main.java
@@ -2,10 +2,24 @@ import pojo.Lavadora;
 
 public class Main {
     public static void main(String[] args) {
-        Lavadora lavadora = new Lavadora (10, "Xiaomi");
-        Lavadora lavadora2 = new Lavadora (16, "Balay");
-        Lavadora lavadora3 = new Lavadora (10, "Xiaomi");
+        Lavadora lavadora1 = new Lavadora(10, "Xiaomi");
+        Lavadora lavadora2 = new Lavadora(16, "Balay");
+        Lavadora lavadora3 = new Lavadora(10, "Xiaomi");
 
-        abrirPuerta(lavadora);
+        lavadora1.abrirPuerta();
+        lavadora2.abrirPuerta();
+        lavadora3.abrirPuerta();
+
+        lavadora1.cargar(8);
+        lavadora3.cargar(12);
+
+
+        lavadora1.programarLavado("Delicado");
+        lavadora2.programarLavado("Intensivo");
+
+        lavadora1.iniciarLavado();
+        lavadora3.iniciarLavado();
+
+        lavadora1.abrirPuerta();
     }
 }
--- 005 UML Sistema Lavadora/src/pojo/Lavadora.java
+++ 005 UML Sistema Lavadora/src/pojo/Lavadora.java
@@ -1,22 +1,69 @@
 package pojo;
 
 public class Lavadora {
-
     private int capacidad;
     private String modelo;
-    private String puerta = "cerrada";
+    private String puerta;
+    private String programaLavado;
+    private boolean enLavado;
+    private int cargaActual;
 
-    public Lavadora (int capacidad, String modelo) {
+    public Lavadora(int capacidad, String modelo) {
         this.capacidad = capacidad;
         this.modelo = modelo;
+        this.puerta = "cerrada";
+        this.enLavado = false;
+        this.cargaActual = 0;
     }
 
-    private void abrirPuerta(Lavadora lavadora) {
-        if (puerta == "cerrada") {
-            System.out.println("Se ha abierto la puerta");
+    public void abrirPuerta() {
+        if (enLavado) {
+            System.out.println("Tontico durante el lavado no se puede abrir la puerta");
+        } else if ("cerrada".equals(puerta)) {
             puerta = "abierta";
-        }else {
-            System.out.println("La puerta ya esta abierta");
+            System.out.println("Puerta abierta");
+        } else {
+            System.out.println("Tontico la puerta ya está abierta");
         }
+    }
+
+    public void cargar(int kilos) {
+        if (!"cerrada".equals(puerta)) {
+            if (kilos <= capacidad) {
+                cargaActual = kilos;
+                System.out.println("Cargados " + kilos + " kg.");
+            } else {
+                System.out.println("Carga excedida. Capacidad máxima: " + capacidad + " kg");
+            }
+        } else {
+            System.out.println("Tienes que abrir la puerta tontico");
+        }
+    }
+
+    public void programarLavado(String programa) {
+        programaLavado = programa;
+        System.out.println("Lavado programado: " + programa);
+    }
+
+    public void iniciarLavado() {
+        if (cargaActual > 0) {
+            enLavado = true;
+            puerta = "cerrada";
+            System.out.println("Iniciando programa de lavado: " + programaLavado);
+            terminarLavado();
+        } else {
+            System.out.println("No se puede iniciar el lavado no la has cargado");
+        }
+    }
+
+    private void terminarLavado() {
+        System.out.println("Lavado en curso...");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        enLavado = false;
+        System.out.println("Lavado finalizado.");
     }
 }
