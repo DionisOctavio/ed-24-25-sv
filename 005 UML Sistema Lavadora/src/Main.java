f2c811726e26bed1448a07f612c86866725bb672
DionisOctavio <dionisoctavioalvarezescorcia@gmail.com>
2025-01-21 22:28:22 +0100

    [21/01/2025] 22:26
    [21/01/2025] 22:26

--- 005 UML Sistema Lavadora/src/Main.java
+++ 005 UML Sistema Lavadora/src/Main.java
@@ -2,7 +2,10 @@ import pojo.Lavadora;
 
 public class Main {
     public static void main(String[] args) {
-        Lavadora lavadora = new Lavadora ();
+        Lavadora lavadora = new Lavadora (10, "Xiaomi");
+        Lavadora lavadora2 = new Lavadora (16, "Balay");
+        Lavadora lavadora3 = new Lavadora (10, "Xiaomi");
 
+        abrirPuerta(lavadora);
     }
 }
--- 005 UML Sistema Lavadora/src/pojo/Lavadora.java
+++ 005 UML Sistema Lavadora/src/pojo/Lavadora.java
@@ -2,8 +2,21 @@ package pojo;
 
 public class Lavadora {
 
+    private int capacidad;
+    private String modelo;
+    private String puerta = "cerrada";
 
-    private void Lavadora (){
-        
+    public Lavadora (int capacidad, String modelo) {
+        this.capacidad = capacidad;
+        this.modelo = modelo;
+    }
+
+    private void abrirPuerta(Lavadora lavadora) {
+        if (puerta == "cerrada") {
+            System.out.println("Se ha abierto la puerta");
+            puerta = "abierta";
+        }else {
+            System.out.println("La puerta ya esta abierta");
+        }
     }
 }
